75c1b34343d53de6d9ada198ffe90b16f2569366
makso8makso <igor.maksimovic@soprex.com>
2025-03-04 22:27:20 +0100

    add paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(3);
 
         return view('admin.index', compact('users'));
     }
